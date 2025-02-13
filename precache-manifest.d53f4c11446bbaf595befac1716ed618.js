4bc965ee97f5f47c4d1210b9df36caee318e7051
Nyirfa Bálint <nyirfab@idomsoft.hu>
2025-02-13 10:49:18 +0100

    Updates

---
+++ precache-manifest.d53f4c11446bbaf595befac1716ed618.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae5e302aab384ce3a429fd6fe0a29e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2dfb45a6c5d54a1d749",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "638e200e69d950262938",
+    "url": "/static/js/2.ae026ab9.chunk.js"
+  },
+  {
+    "revision": "f457a91e29f5e6cff1f0",
+    "url": "/static/js/3.6757c1ca.chunk.js"
+  },
+  {
+    "revision": "a2dfb45a6c5d54a1d749",
+    "url": "/static/js/main.b213d3d3.chunk.js"
+  },
+  {
+    "revision": "e9ff480946dbdebe545e",
+    "url": "/static/js/runtime~main.d663dc0a.js"
+  }
+]);
